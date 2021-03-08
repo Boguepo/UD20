f11eb26cef8fbf1ae24ab4c49306a3d305c1327a
Boguepo <46709093+Boguepo@users.noreply.github.com>
2021-03-08 17:38:37 +0100

    Fixed

--- ej8/src/main/java/operaciones/Operacion.java
+++ ej8/src/main/java/operaciones/Operacion.java
@@ -2,6 +2,7 @@ package operaciones;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
 import java.util.InputMismatchException;
 
 import javax.swing.JOptionPane;
@@ -11,59 +12,55 @@ import visual.Visual;
 public class Operacion implements ActionListener {
 
 	private Visual v;
-	private final double factorE  = 166.386;
-	private final double factorP = 0.006; 
+	private final double factorE = 166.386;
+	private final double factorP = 0.006;
+	private DecimalFormat form = new DecimalFormat("0.##");
 
 	public Operacion(Visual v) {
 		this.v = v;
 	}
-	//metodo que incia el programa dandole los listeners a los botones
+
+	// metodo que incia el programa dandole los listeners a los botones
 	public void incio() {
 		v.getConvertir().addActionListener(this);
 		v.getCambiar().addActionListener(this);
 		v.getBorrar().addActionListener(this);
-		
-		
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		//para validar que boton se ha clickado uso la funcion getActionCommand que me dice el nombre del boton que se ha pulsado
+		// para validar que boton se ha clickado uso la funcion getActionCommand que me
+		// dice el nombre del boton que se ha pulsado
 		// de esta manera puedes abrupar todos los listeners en la misma funcion
-		
-		if(v.getCambiar().getText() == e.getActionCommand()) {
-			if(v.getConvertir().getText() == "Ptas a Euros") {
+
+		if (v.getCambiar().getText() == e.getActionCommand()) {
+			if (v.getConvertir().getText() == "Ptas a Euros") {
 				v.getConvertir().setText("Euros a Ptas");
-			}else {
+			} else {
 				v.getConvertir().setText("Ptas a Euros");
 			}
-			
-		}else if(v.getConvertir().getText() == e.getActionCommand()) {
+			v.clickCambiar();
+		} else if (v.getConvertir().getText() == e.getActionCommand()) {
 			try {
-				if(v.getConvertir().getText() == "Ptas a Euros") {
-				double result = Integer.parseInt(v.getNum1().getText()) * factorP;
-				v.getResultado().setText(Double.toString(result));
-			}else {
-				double result = Integer.parseInt(v.getNum1().getText()) * factorE;
-				v.getResultado().setText(Double.toString(result));
-			}
-			
+				if (v.getConvertir().getText() == "Ptas a Euros") {
+					double result = Integer.parseInt(v.getNum1().getText()) * factorP;
+					v.getResultado().setText(form.format(result));
+				} else {
+					double result = Integer.parseInt(v.getNum1().getText()) * factorE;
+					v.getResultado().setText(form.format(result));
+				}
+
 			} catch (InputMismatchException e2) {
 				JOptionPane.showMessageDialog(null, "Valor erroneo introducido");
 			} catch (Exception e3) {
 				JOptionPane.showMessageDialog(null, "Valor erroneo introducido");
 			}
-			
-						
-						
-			}else {
-				v.getNum1().setText("");
-				v.getResultado().setText("");
-			}
+
+		} else {
+			v.getNum1().setText("");
+			v.getResultado().setText("");
 		}
-		
 	}
-	
-	
 
-
+}
